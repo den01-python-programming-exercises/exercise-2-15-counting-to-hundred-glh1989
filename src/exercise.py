54ff85df2a12f713ef737a599e000eb4111a5fa4
gh1989 <>
2021-04-10 20:39:29 +0000

    attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
-    #write your code below this line
+    number = int(input("Give an Integer "))
+
+    while(number < 101):
+       print(number)
+       number +=1
 
 if __name__ == '__main__':
     main()
+
